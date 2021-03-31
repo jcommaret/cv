da0969f41f3e3865e2db29220f6f419d15841a38
Jérôme Commaret <jcommaret@gmail.com>
2021-03-31 04:20:09 +0200

    Added and externalised slider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import logo from './img/moi.JPG';
 import './App.scss';
+import './Animation.scss';
+import SimpleSlider from './Slider'
 
 function App() {
   return (
@@ -19,7 +21,7 @@ function App() {
           <p>📚 : 2021 (will have certificate)<b>-</b> <a href="https://www.edx.org">Georgia Tech - Innovation Leadership</a></p>
           <p>📝 : Since 2016 : Blogging about <a href="https://medium.com/meteor-in-french">Meteor</a></p>
           <p>👨‍💻 : Meteor, if not : Wordpress, Symfony, React.js, React-native, Angular...</p>
-          <p>⚙ : TypeScript, Javascript, jQuery, Git, Gulp, Webpack, php, scss, sass, all struff like that</p>
+          <p>⚙️ : TypeScript, Javascript, jQuery, Git, Gulp, Webpack, php, scss, sass, all struff like that</p>
         </div>
 
         <div className="block">  
@@ -31,43 +33,19 @@ function App() {
         <div className="block">
           <h3>Others stuffs</h3>
           <p>🦁 : LinkedIn Open Networker - 4k+ : <a href="https://fr.linkedin.com/in/jcommaret">LinkedIn</a></p>
-          <h3>Recommandations</h3>
-
-          <figure>
-            <blockquote>
-              <p><cite>"Ce fut un plaisir de travailler avec Jerome dans le cadre de la plateforme Preditt. Ses compétences et son écoute ont permis d'atteindre nos objectifs !"</cite></p>
-            </blockquote>
-            <figcaption>Julien - CEO - Preditt</figcaption>
-          </figure>
-
-          <figure>
-            <blockquote>
-              <p><cite>"I had the pleasure to work with Jérôme and if you need someone who has great managerial skills and who is also a technical profile, you should definitely hire him. His knowledge of the production chain makes him great at doing what he does. He is a person who can adapt and learn quickly, and his mobile / e-commerce and localisation skills, makes him a valuable person in every teams, to build really great digital product."</cite></p>
-            </blockquote>
-            <figcaption>Rhose - Product Owner - The Coca Cola Company</figcaption>
-          </figure>
-
-          <figure>
-            <blockquote>
-              <p><cite>"Je n'ai pas rencontré beaucoup de personnes comme Jérôme dans ma carrière. J’ai eu l'occasion de travailler avec lui pendant 6 mois alors qu'il était "Web and Mobile Project Manager" chez Régime Coach et nous avons collaboré sur des projets d’envergure. C'est un geek assumé, qui paradoxalement a su faire preuve d'une grande pédagogie et de proximité avec les équipes métiers. Très au fait de l'écosystème dans lequel il a développé ses domaines de compétences, Jérôme est un expert en gestion de projets digitaux tant sur le plan technique que stratégique. Il est également capable d'une grande adaptabilité qui peut se révéler un véritable atout pour toute entreprise qui l'emploierait."</cite></p>
-            </blockquote>
-            <figcaption>Nathalie - Directrice Europe des Services de Coaching - Groupe Dukan</figcaption>
-          </figure>
-          
-          <figure>
-            <blockquote>
-              <p><cite>"J'ai toujours connu Jérôme avec des idées fraîches, créatives et ambitieuses. Nous avons eu l'occasion de travailler ensemble pour le compte d'un de mes projets, sur des problématiques techniques et marketing pointues. Faire l'expérience de ses talents et expertises n'a pas été une surprise, Jérôme est quelqu'un de vraiment très très fiable et passionné par son métier. C'est le genre de personne qui pousse les autres vers le haut et qui le fait avec beaucoup de sentiments et de modestie."</cite></p>
-            </blockquote>
-            <figcaption>Camille - CEO - Galopeo</figcaption>
-          </figure>
         </div>
-        
+
         <div className="block">  
           <h3>Leisure :</h3>
-          <p>🏆 : Photograph </p>
-          <p>🏆 : Model Bodypositive</p>
+          <p>📸 : Photograph </p>
+          <p>🙋‍♂️ : Model Bodypositive</p>
+          <p>🎸 : Attended to many music festival</p>
+          <p>🎮 : Mario Kart, Zelda, Need For Speed Hot Pursuit, Mario Odyssey</p>
+          <p>🎨 : Enjoy museum, street Arts</p>
+          <p>🎭 : Played tragedy and comedy </p>
         </div>
         
+        <SimpleSlider></SimpleSlider> 
       </div>
     </div>
   );
---
+++ src/Slider.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Slider from "react-slick";
+import './Slider.scss';
+
+
+export default function SimpleSlider() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+        <div className="block"> 
+             <h3>Recommandations</h3>
+                <Slider {...settings}>
+                <div>
+                    <figure>
+                        <blockquote>
+                            <p><cite>"I had the pleasure to work with Jérôme and if you need someone who has great managerial skills and who is also a technical profile, you should definitely hire him. His knowledge of the production chain makes him great at doing what he does. He is a person who can adapt and learn quickly, and his mobile / e-commerce and localisation skills, makes him a valuable person in every teams, to build really great digital product."</cite></p>
+                        </blockquote>
+                        <figcaption>Rhose - Product Owner - The Coca Cola Company</figcaption>
+                    </figure>
+                </div>
+                <div>
+                    <figure>
+                        <blockquote>
+                            <p><cite>"Ce fut un plaisir de travailler avec Jerome dans le cadre de la plateforme Preditt. Ses compétences et son écoute ont permis d'atteindre nos objectifs !"</cite></p>
+                        </blockquote>
+                        <figcaption>Julien - CEO - Preditt</figcaption>
+                    </figure>
+                </div>
+                <div>
+                    <figure>
+                        <blockquote>
+                        <p><cite>"Je n'ai pas rencontré beaucoup de personnes comme Jérôme dans ma carrière. J’ai eu l'occasion de travailler avec lui pendant 6 mois alors qu'il était "Web and Mobile Project Manager" chez Régime Coach et nous avons collaboré sur des projets d’envergure. C'est un geek assumé, qui paradoxalement a su faire preuve d'une grande pédagogie et de proximité avec les équipes métiers. Très au fait de l'écosystème dans lequel il a développé ses domaines de compétences, Jérôme est un expert en gestion de projets digitaux tant sur le plan technique que stratégique. Il est également capable d'une grande adaptabilité qui peut se révéler un véritable atout pour toute entreprise qui l'emploierait."</cite></p>
+                        </blockquote>
+                        <figcaption>Nathalie - Directrice Europe des Services de Coaching - Groupe Dukan</figcaption>
+                    </figure>
+                </div>
+                <div>        
+                    <figure>
+                    <blockquote>
+                    <p><cite>"J'ai toujours connu Jérôme avec des idées fraîches, créatives et ambitieuses. Nous avons eu l'occasion de travailler ensemble pour le compte d'un de mes projets, sur des problématiques techniques et marketing pointues. Faire l'expérience de ses talents et expertises n'a pas été une surprise, Jérôme est quelqu'un de vraiment très très fiable et passionné par son métier. C'est le genre de personne qui pousse les autres vers le haut et qui le fait avec beaucoup de sentiments et de modestie."</cite></p>
+                    </blockquote>
+                    <figcaption>Camille - CEO - Galopeo</figcaption>
+                </figure>
+                </div>
+            </Slider>
+        </div>
+    );
+  }
